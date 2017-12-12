d0fe846f8b264b0f0db7c69480e4488625bb9322
Uthaeus <Uth@github.com>
2017-12-12 09:55:13 -0700

    Homers tool initial commit

---
+++ lib/homers_view_tool.rb
@@ -0,0 +1,6 @@
+require "homers_view_tool/version"
+require "homers_view_tool/renderer"
+
+module HomersViewTool
+  # Your code goes here...
+end
---
+++ lib/homers_view_tool/version.rb
@@ -0,0 +1,3 @@
+module HomersViewTool
+  VERSION = "0.1.0"
+end
